db18376fc7557ab1b38811bc8bb5feae079fdafc
Dirkjan Ochtman <dirkjan@ochtman.nl>
2022-07-25 16:01:03 +0200

    Clarify section state

--- src/pemfile.rs
+++ src/pemfile.rs
@@ -1,5 +1,6 @@
-use base64;
 use std::io::{self, ErrorKind};
+
+use base64;
 
 /// The contents of a single recognised block in a PEM file.
 #[non_exhaustive]
@@ -40,8 +41,7 @@ impl Item {
 /// `for item in iter::from_fn(|| read_one(rd).transpose()) { ... }`
 pub fn read_one(rd: &mut dyn io::BufRead) -> Result<Option<Item>, io::Error> {
     let mut b64buf = String::with_capacity(1024);
-    let mut section_type = None;
-    let mut end_marker = None;
+    let mut section = None;
     let mut line = String::with_capacity(80);
 
     loop {
@@ -50,48 +50,43 @@ pub fn read_one(rd: &mut dyn io::BufRead) -> Result<Option<Item>, io::Error> {
 
         if len == 0 {
             // EOF
-            if end_marker.is_some() {
-                return Err(io::Error::new(
+            return match section {
+                Some((_, end_marker)) => Err(io::Error::new(
                     ErrorKind::InvalidData,
-                    format!("section end {:?} missing", end_marker.unwrap()),
-                ));
-            }
-            return Ok(None);
+                    format!("section end {:?} missing", end_marker),
+                )),
+                None => Ok(None),
+            };
         }
 
         if line.starts_with("-----BEGIN ") {
-            let trailer = line[11..]
-                .find("-----")
-                .ok_or_else(|| {
-                    io::Error::new(
-                        ErrorKind::InvalidData,
-                        format!("illegal section start: {:?}", line),
-                    )
-                })?;
+            let trailer = line[11..].find("-----").ok_or_else(|| {
+                io::Error::new(
+                    ErrorKind::InvalidData,
+                    format!("illegal section start: {:?}", line),
+                )
+            })?;
 
             let ty = &line[11..11 + trailer];
-
-            section_type = Some(ty.to_string());
-            end_marker = Some(format!("-----END {}-----", ty).to_string());
+            section = Some((ty.to_string(), format!("-----END {}-----", ty).to_string()));
             continue;
         }
 
-        if end_marker.is_some() && line.starts_with(end_marker.as_ref().unwrap()) {
-            let der = base64::decode(&b64buf)
-                .map_err(|err| io::Error::new(ErrorKind::InvalidData, err))?;
+        if let Some((section_type, end_marker)) = section.as_ref() {
+            if line.starts_with(end_marker) {
+                let der = base64::decode(&b64buf)
+                    .map_err(|err| io::Error::new(ErrorKind::InvalidData, err))?;
 
-            let item = Item::from_start_line(&section_type.unwrap(), der);
-
-            if let Some(item) = item {
-                return Ok(Some(item));
-            } else {
-                section_type = None;
-                end_marker = None;
-                b64buf.clear();
+                if let Some(item) = Item::from_start_line(&section_type, der) {
+                    return Ok(Some(item));
+                } else {
+                    section = None;
+                    b64buf.clear();
+                }
             }
         }
 
-        if section_type.is_some() {
+        if section.is_some() {
             b64buf.push_str(line.trim());
         }
     }
