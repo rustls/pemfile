8b0bd30b71705a2b87a11ac8d3b1deee78ed7af7
Joe Birr-Pixton <jpixton@gmail.com>
2024-08-15 18:36:28 +0100

    Extend benchmarking
    As well as the existing one, benchmark reading zen.pem and
    a typical client operation (reading /etc/ssl/certs/ca-certificates.crt).
    
    Use criterion for local workflow (tracking results over changes).

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,20 +1,45 @@
 use std::io::BufReader;
 
-use bencher::{benchmark_group, benchmark_main, Bencher};
+use criterion::{criterion_group, criterion_main, Criterion};
 
-fn criterion_benchmark(c: &mut Bencher) {
-    c.iter(|| {
-        let data = include_bytes!("../tests/data/certificate.chain.pem");
-        let mut reader = BufReader::new(&data[..]);
-        assert_eq!(
+fn bench(c: &mut Criterion) {
+    // typical server workload
+    c.bench_function("3 certificates", |b| {
+        b.iter(|| {
+            let data = include_bytes!("../tests/data/certificate.chain.pem");
+            let mut reader = BufReader::new(&data[..]);
+            assert_eq!(
+                rustls_pemfile::certs(&mut reader)
+                    .collect::<Result<Vec<_>, _>>()
+                    .unwrap()
+                    .len(),
+                3
+            );
+        })
+    });
+
+    // one of every supported type
+    c.bench_function("zen", |b| {
+        b.iter(|| {
+            let data = include_bytes!("../tests/data/zen.pem");
+            let mut reader = BufReader::new(&data[..]);
+            rustls_pemfile::read_all(&mut reader)
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap();
+        })
+    });
+
+    // typical client workload
+    c.bench_function("ca-certs", |b| {
+        b.iter(|| {
+            let data = include_bytes!("../tests/data/ca-certs.pem");
+            let mut reader = BufReader::new(&data[..]);
             rustls_pemfile::certs(&mut reader)
                 .collect::<Result<Vec<_>, _>>()
-                .unwrap()
-                .len(),
-            3
-        );
+                .unwrap();
+        })
     });
 }
 
-benchmark_group!(benches, criterion_benchmark);
-benchmark_main!(benches);
+criterion_group!(benches, bench);
+criterion_main!(benches);
